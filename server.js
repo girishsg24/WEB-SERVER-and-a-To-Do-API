8da3e6be3c4d186f084be849c8889d0eb30f5c56
Girish S G <gangappa.girish@gmail.com>
2016-11-28 13:07:42 -0800

    /todo update

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 var express=require('express');
 var app=express();
 var PORT=process.env.PORT||3000;
+var todos=[
+			{
+				id:1,
+				description:'Call mom in the morning.',
+				completed:false
+
+			},
+			{
+				id:2,
+				description:'Be Batman & save somebody!',
+				completed:false
+			},
+			{
+				id:3,
+				description:'Be Awesome!',
+				completed:true
+			}
+
+
+		];
+
 app.get('/',
 	function(req,res)
 	{
@@ -8,6 +29,29 @@ app.get('/',
 	}
 );
 
+app.get('/todos',
+		function(req,res)
+		{
+			res.json(todos);
+		}
+	);
+app.get('/todos/:id',
+		function(req,res)
+		{
+			var matchedTodoItem;
+			todos.forEach(
+				function(curTodo)
+				{
+					if (curTodo.id==req.params.id)
+						matchedTodoItem=curTodo;
+				}
+			);
+			if (matchedTodoItem)
+				res.json(matchedTodoItem);
+			else
+				res.status(404).send('Page Not found Ya!');
+		}
+	);
 app.listen(PORT,
 			function()
 			{
